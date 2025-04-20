0188a241c25e1efbcd4a18a6800a0958cc57de58
r1xez <164931719+r1xez@users.noreply.github.com>
2025-04-20 12:58:20 +0300

    completed

--- 20.04.01/Program.cs
+++ 20.04.01/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+   
+    public delegate void MessageDelegate(string message);
+
+   
+    public static void PrintMessage(string message)
+    {
+        Console.WriteLine($"Console Output: {message}");
+    }
+
+   
+    public static void WriteMessageToFile(string message)
+    {
+        string filePath = "output.txt";
+        File.AppendAllText(filePath, message + Environment.NewLine);
+        Console.WriteLine($"Message written to file: {filePath}");
+    }
+
+    static void Main()
+    {
+        MessageDelegate messageDelegate = PrintMessage;
+        messageDelegate += WriteMessageToFile;           
+
+       
+        string message = "Hello, this is a test message!";
+        messageDelegate(message);
+
+       
+        string fileContent = File.ReadAllText("output.txt");
+        Console.WriteLine("File content:\n" + fileContent);
+    }
+}
