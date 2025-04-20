9263a2c5168d23e5468451a3c27418944b6a1224
r1xez <164931719+r1xez@users.noreply.github.com>
2025-04-20 12:57:05 +0300

    completed

--- 20.04.01/Program.cs
+++ 20.04.01/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class StringProcessor
+{
+   
+    public delegate int StringOperation(string str);
+
+   
+    public static int CountVowels(string str)
+    {
+        int count = 0;
+        foreach (char c in str)
+        {
+            if ("aeiouAEIOU".Contains(c))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+   
+    public static int CountConsonants(string str)
+    {
+        int count = 0;
+        foreach (char c in str)
+        {
+            if (Char.IsLetter(c) && !"aeiouAEIOU".Contains(c))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    
+    public static int CountLength(string str)
+    {
+        return str.Length;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = "Hello World!";
+
+       
+        StringProcessor.StringOperation vowelsDelegate = new StringProcessor.StringOperation(StringProcessor.CountVowels);
+        StringProcessor.StringOperation consonantsDelegate = new StringProcessor.StringOperation(StringProcessor.CountConsonants);
+        StringProcessor.StringOperation lengthDelegate = new StringProcessor.StringOperation(StringProcessor.CountLength);
+
+       
+        int vowelsCount = vowelsDelegate(inputString);
+        int consonantsCount = consonantsDelegate(inputString);
+        int length = lengthDelegate(inputString);
+
+        Console.WriteLine($"Input string: {inputString}");
+        Console.WriteLine($"Number of vowels: {vowelsCount}");
+        Console.WriteLine($"Number of consonants: {consonantsCount}");
+        Console.WriteLine($"Length of string: {length}");
+    }
+}
